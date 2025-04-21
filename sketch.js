8b4fc72cb96c7db339b77989fd4f84ac4aca296a
jcdrawstrash <jd.cunningham2004@gmail.com>
2025-04-21 14:19:17 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+let x,y,ix,iy;
+
+function setup() {
+  createCanvas(500, 500);
+  noStroke();
+}
+
+function draw() {
+   x=mouseX;
+  y=mouseY;
+  ix=width-mouseX;
+  iy=height-mouseY;
+  
+  background(200);
+  fill(255, 150);
+  ellipse(x,x,x,x);
+  fill(0,159);
+  ellipse(ix,ix,ix,ix);
+}
